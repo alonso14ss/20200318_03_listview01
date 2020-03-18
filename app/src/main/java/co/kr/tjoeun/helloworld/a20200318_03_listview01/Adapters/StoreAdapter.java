1d269909c738d5dcb222d22b760a5754082ae11b
alonso14ss <reiyan92@gmail.com>
2020-03-18 12:40:56 +0900

    목록 / 어댑터 변수 객체화  - 리스트뷰와 연결   - 목록에 가게추가    - 리스트에

--- app/src/main/java/co/kr/tjoeun/helloworld/a20200318_03_listview01/Adapters/StoreAdapter.java
+++ app/src/main/java/co/kr/tjoeun/helloworld/a20200318_03_listview01/Adapters/StoreAdapter.java
@@ -2,12 +2,16 @@ package co.kr.tjoeun.helloworld.a20200318_03_listview01.Adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.List;
 
+import co.kr.tjoeun.helloworld.a20200318_03_listview01.R;
 import co.kr.tjoeun.helloworld.a20200318_03_listview01.datas.Store;
 
 // ALT + ENTER => 에러 해결 제안
@@ -26,5 +30,17 @@ public class StoreAdapter extends ArrayAdapter<Store> {
         mList = objects;
         inf = LayoutInflater.from(mContext);
 
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View row = convertView; // row는 지금 그려져야 할 줄을 답는다
+
+        if(row == null){
+//            기존에 그려둔게 없어서(null) 새로 그려야 하나?
+            row= inf.inflate(R.layout.store_list_item,null);
+        }
+        return row;
     }
 }
--- app/src/main/java/co/kr/tjoeun/helloworld/a20200318_03_listview01/MainActivity.java
+++ app/src/main/java/co/kr/tjoeun/helloworld/a20200318_03_listview01/MainActivity.java
@@ -5,9 +5,19 @@ import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import co.kr.tjoeun.helloworld.a20200318_03_listview01.Adapters.StoreAdapter;
 import co.kr.tjoeun.helloworld.a20200318_03_listview01.databinding.ActivityMainBinding;
+import co.kr.tjoeun.helloworld.a20200318_03_listview01.datas.Store;
 
 public class MainActivity extends BaseActivity {
+
+//    가게 목록이 담길 리스트
+    List<Store> storeDatas = new ArrayList<>();
+//    목록을 뿌려줄 스토어어댑터 변수
+    StoreAdapter storeAdapter = null;
 
     ActivityMainBinding binding = null;
 
@@ -24,8 +34,22 @@ public class MainActivity extends BaseActivity {
 
     }
 
+
+
     @Override
     public void setValues() {
+        addStores();
+
+//        어댑터를 객체화 (우리가 작성한 생성자 활용)
+        storeAdapter = new StoreAdapter(mContext, R.layout.store_list_item, storeDatas);
+//        리스트뷰와 어댑터를 연결
+        binding.storeListView.setAdapter(storeAdapter);
+
+    }
+
+    void addStores() {
+        storeDatas.add(new Store("삼겹살더하기 본점", 4));
+        storeDatas.add(new Store("밥해주는 남자", 3));
 
     }
 }
