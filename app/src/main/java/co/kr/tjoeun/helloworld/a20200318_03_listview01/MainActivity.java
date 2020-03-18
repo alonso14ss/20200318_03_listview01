5dcb1c940bddda1cd9b7bcce1fe39e77b97f5db6
alonso14ss <reiyan92@gmail.com>
2020-03-18 11:33:44 +0900

    Initial commit

---
+++ app/src/main/java/co/kr/tjoeun/helloworld/a20200318_03_listview01/MainActivity.java
@@ -0,0 +1,31 @@
+package co.kr.tjoeun.helloworld.a20200318_03_listview01;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import co.kr.tjoeun.helloworld.a20200318_03_listview01.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
